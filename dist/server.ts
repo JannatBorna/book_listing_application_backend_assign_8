4ba504365ec2111fb234baebee25a842cce1912f
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-09-18 16:21:12 +0600

    add

--- dist/server.ts
+++ dist/server.ts
@@ -1,37 +1,16 @@
-import { Server } from 'http';
-// import app from './app';
-import config from './config';
-import { errorlogger, logger } from './shared/logger';
-
-async function bootstrap() {
-  const server: Server = app.listen(config.port, () => {
-    logger.info(`Server running on port ${config.port}`);
-    console.log(`🛢   Database is connected successfully`);
-  });
-
-  const exitHandler = () => {
-    if (server) {
-      server.close(() => {
-        logger.info('Server closed');
-      });
-    }
-    process.exit(1);
-  };
-
-  const unexpectedErrorHandler = (error: unknown) => {
-    errorlogger.error(error);
-    exitHandler();
-  };
-
-  process.on('uncaughtException', unexpectedErrorHandler);
-  process.on('unhandledRejection', unexpectedErrorHandler);
-
-  process.on('SIGTERM', () => {
-    logger.info('SIGTERM received');
-    if (server) {
-      server.close();
-    }
-  });
-}
-
-bootstrap();
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const logger_1 = require('./utilities/logger');
+const bootStrap_1 = require('./utilities/bootStrap');
+let server;
+process.on('uncaughtException', error => {
+  logger_1.errorLogger.error(error);
+  process.exit(1);
+});
+(0, bootStrap_1.bootStrap)();
+process.on('SIGTERM', () => {
+  logger_1.logger.info(`Sigterm is received`);
+  if (server) {
+    server.close();
+  }
+});
